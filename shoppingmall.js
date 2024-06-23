6fcc401d0268b9d0d6fe7a70d98198417d0517b5
sehyeon bang <tpwnstpgus1@naver.com>
2024-06-23 21:46:24 +0900

    q1 4차 제출

--- shoppingmall.js
+++ shoppingmall.js
@@ -1,37 +1,29 @@
 // shoppingmall.js
 import fs from "fs";
 
-
-
-
 export class ShoppingMall {
-	constructor() {
-		this.items = this.loadItems(); // loadItems() 메서드 이용하여 json파일의 데이터를 받아옴
-	}
+  constructor() {
+    this.items = this.loadItems(); // loadItems() 메서드 이용하여 json파일의 데이터를 받아옴
+  }
 
-	loadItems() {
-		try {
-			const data = fs.readFileSync("./items.json", "utf8");
-			return JSON.parse(data);
-		} catch (err) {
-			console.error("상품을 불러오는 중 오류가 발생했습니다:", err);
-			return [];
-		}
-	}
+  loadItems() {
+    try {
+      const data = fs.readFileSync("./items.json", "utf8");
+      return JSON.parse(data);
+    } catch (err) {
+      console.error("상품을 불러오는 중 오류가 발생했습니다:", err);
+      return [];
+    }
+  }
 
-	cleanItems(it){
-		let count=0;
-		Object.entries(this.items).forEach(([name, price]) => {
-			if (it===name){
-				console.log(`${it}: 아이템이 판매되었습니다.`);
-				count+=1;
-				delete this.items[it];
-			}
-		});
-		if (count===0){
-			console.log('해당 아이템은 존재하지 않습니다.');
-		}
-	}
-
+  cleanItems(it) {
+    for (const [name, price] of Object.entries(this.items)) {
+      if (it === name) {
+        console.log(`${it}: 아이템이 판매되었습니다.`);
+        delete this.items[it];
+        return;
+      }
+    }
+    console.log("해당 아이템은 존재하지 않습니다.");
+  }
 }
-
--- view.js
+++ view.js
@@ -1,54 +1,48 @@
 // view.js
 import fs from "fs";
-import readline from 'readline';
+import readline from "readline";
 import { ShoppingMall } from "./shoppingmall.js";
 
-const r1 = readline.createInterface({ // 입출력을 위한 인터페이스를 생성
-	input: process.stdin,
-	output: process.stdout
+const r1 = readline.createInterface({
+  // 입출력을 위한 인터페이스를 생성
+  input: process.stdin,
+  output: process.stdout,
 });
 
 export class View {
-    // 주문 가능한 상품 목록 출력 함수
-    displayAvailableItems(items) {
-      let count=1;
-      console.log("주문 가능한 상품:");
-      Object.entries(items).forEach(([name, price]) => {
-        console.log(`${count}. ${name}: ${price.toLocaleString()}원`);
-        count++;
+  // 주문 가능한 상품 목록 출력 함수
+  displayAvailableItems(items) {
+    let count = 1;
+    console.log("주문 가능한 상품:");
+    Object.entries(items).forEach(([name, price]) => {
+      console.log(`${count}. ${name}: ${price.toLocaleString()}원`);
+      count++;
+    });
+  }
+
+  a;
+
+  callItems() {
+    return new Promise(function (resolve, reject) {
+      console.log("주문할 상품을 입력하세요 >");
+
+      // 'line' 이벤트 리스너 등록
+      r1.on("line", function (str) {
+        resolve(str); // 'line' 이벤트 발생 시 입력된 문자열을 resolve하여 처리
       });
-    }
 
-    a
-
-    callItems() {
-      return new Promise(function(resolve, reject) {
-        console.log('주문할 상품을 입력하세요 >');
-        
-        // 'line' 이벤트 리스너 등록
-        r1.on('line', function(str) {
-          resolve(str); // 'line' 이벤트 발생 시 입력된 문자열을 resolve하여 처리
-        });
-    
-        // 에러 이벤트 리스너 등록
-        r1.on('error', function(err) {
-          reject(err); // 에러 발생 시 reject를 호출하여 처리
-        });
+      // 에러 이벤트 리스너 등록
+      r1.on("error", function (err) {
+        reject(err); // 에러 발생 시 reject를 호출하여 처리
       });
+    });
+  }
+
+  async checkproduct(items) {
+    try {
+      return await this.callItems();
+    } catch {
+      return "error!";
     }
-
-
-
-    async checkproduct(items){
-      try{
-        return await this.callItems();
-      } catch{
-        return 'error!'
-      }
-      } 
-
-    
-    
+  }
 }
-
-
